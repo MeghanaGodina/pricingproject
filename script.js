0bdd0e01abb38988bf688ce89f2d1506e6f94c56
MeghanaGodina <117910465+MeghanaGodina@users.noreply.github.com>
2024-01-30 21:53:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function selectPlan(plan) {
+    alert('You have selected the ' + plan + ' plan. Redirect to the signup page or perform necessary actions.');
+    // Add logic to redirect to signup page or perform other actions based on the selected plan.
+}
